7992d30f9cc9cf07c2e3c94278ee0dba6813a359
Devin McAnelly <devin@atlantis.lan>
2024-09-22 22:06:11 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	deleted:    .alacritty.toml 	new file:   .config/emacs/custom-vars.el 	new file:   .config/emacs/init.el 	deleted:    .config/helix/ 	deleted:    .config/hypr/ 	deleted:    .config/neofetch/ 	deleted:    .config/spotifyd 	deleted:    .config/waybar/ 	modified:   .gitignore 	deleted:    .vimrc 	renamed:    README.md -> README.org
    I have ascended. The days of blindly modal editing in the dark with the unwashed NeoVim masses, are over.
    No longer will my RAM be tied up in Microsoft Tetemetry
    Praise be to all things Gnu.
    
    I'm not calling it Gnu/Linux. Bite me.

---
+++ .config/emacs/init.el
@@ -0,0 +1,136 @@
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/"))
+
+      
+      indent-tabs-mode nil
+      python-indent-offset 4
+      use-package-always-ensure t
+      visible-bell t            ;; Flash when the bell rings
+      history-length 25
+      global-auto-revert-mode 1
+      select-enable-clipboard t
+      x-select-enable-clipboard t ;; These should get paste working..
+      x-select-enable-primary t
+      )                         ;; inhibit-startup-message t  // after you finish reading the manual..
+                                ;; or customize the startup message.          
+(menu-bar-mode 1)  ; Leave this one on if you're a beginner!
+(tool-bar-mode 1)
+(scroll-bar-mode -1)
+(hl-line-mode 1)
+(column-number-mode)
+(global-display-line-numbers-mode 1) ;; Display line numbers in every buffer
+(load-theme 'wombat)
+(save-place-mode 1)
+(savehist-mode 1)
+(global-auto-revert-mode 1)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(require 'package)
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
+;; For Python
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs '(python-mode . ("pylsp"))))
+
+;; Org Mode
+(defun dw/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(use-package org
+  :hook (org-mode . dw/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; Replace list hyphen with dot
+(font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(require 'org-indent)
+
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Liberation Mono" :weight 'regular :height (cdr face)))
+
+;; Make sure org-indent face is available
+
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
